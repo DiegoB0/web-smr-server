bb44479df5ceada3d27886de9fe5b2a457e13a7b
Diego Efren Elizalde Barboza <potenciapanadera@gmail.com>
2025-02-17 09:29:06 -0600

    trying to display a simple get

--- src/routes/email.route.js
+++ src/routes/email.route.js
@@ -3,6 +3,10 @@ import { sendEmail } from "../controllers/email.controller.js";
 
 const router = express.Router();
 
+router.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
 router.post("/send-email", sendEmail);
 
 export default router;
